583c82eca337c192a31d5c69995b7bc82c44e62b
fullWaveRectifier <88315678+fullWaveRectifier@users.noreply.github.com>
2021-08-18 00:44:18 +0530

    Added Exception class in UserMS

--- userMS/src/main/java/com/infosys/userms/service/UserServiceImpl.java
+++ userMS/src/main/java/com/infosys/userms/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.infosys.userms.service;
 
 import com.infosys.userms.dto.UserDTO;
 import com.infosys.userms.entity.UserEntity;
+import com.infosys.userms.exception.WeCareException;
 import com.infosys.userms.repository.UserRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +23,7 @@ public class UserServiceImpl implements UserService{
         Optional<UserEntity> user = userRepo.findById(id);
         System.out.println(user.isPresent());
         if(user.isPresent()) return mapper.map(user.get(), UserDTO.class);
-
-        //  Todo: Throw Exceptions
-
-        else return null;
+        else throw new WeCareException("askjfaklsjflasjfkl");
     }
 
 }
