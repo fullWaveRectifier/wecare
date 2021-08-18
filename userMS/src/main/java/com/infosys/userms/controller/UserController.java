1b5b7b48f987507bc135d0c5da300c75c980efeb
Skipper609 <sudhanva000@gmail.com>
2021-08-18 11:16:31 +0530

    Implemented userlogin and coach login

--- userMS/src/main/java/com/infosys/userms/controller/UserController.java
+++ userMS/src/main/java/com/infosys/userms/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.infosys.userms.controller;
 
+import com.infosys.userms.dto.LoginDTO;
 import com.infosys.userms.dto.UserDTO;
+import com.infosys.userms.exception.WeCareException;
 import com.infosys.userms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
@@ -22,5 +26,14 @@ public class UserController {
     ResponseEntity<UserDTO> getUserProfile(@Valid @PathVariable String userId){
         UserDTO user = userService.getUserById(userId);
         return new ResponseEntity<>(user, HttpStatus.FOUND);
+    }
+    @PostMapping("/users/login")
+    ResponseEntity<Boolean> loginUser(@RequestBody LoginDTO login){
+    	try {
+    		Boolean result = userService.userLogin(login);
+			return new ResponseEntity<Boolean>(result,HttpStatus.OK);
+		} catch (WeCareException e) {
+			return new ResponseEntity<Boolean>(false,HttpStatus.OK);
+		}
     }
 }
--- userMS/src/main/java/com/infosys/userms/service/UserServiceImpl.java
+++ userMS/src/main/java/com/infosys/userms/service/UserServiceImpl.java
@@ -1,8 +1,8 @@
 package com.infosys.userms.service;
 
+import com.infosys.userms.dto.LoginDTO;
 import com.infosys.userms.dto.UserDTO;
 import com.infosys.userms.entity.UserEntity;
-import com.infosys.userms.exception.ExceptionConstants;
 import com.infosys.userms.exception.WeCareException;
 import com.infosys.userms.repository.UserRepo;
 import org.modelmapper.ModelMapper;
@@ -23,8 +23,28 @@ public class UserServiceImpl implements UserService{
     @Override
     public UserDTO getUserById(String id) {
         Optional<UserEntity> user = userRepo.findById(id);
+        System.out.println(user.isPresent());
         if(user.isPresent()) return mapper.map(user.get(), UserDTO.class);
-        else throw new WeCareException(ExceptionConstants.USER_NOT_FOUND.toString(), HttpStatus.BAD_REQUEST.value());
+        else throw new WeCareException("askjfaklsjflasjfkl", HttpStatus.OK.value());
+    }
+    
+    @Override
+    public Boolean userLogin(LoginDTO login) {
+    	String id= login.getId();
+    	String password = login.getPassword();
+    	Optional<UserEntity> result =  userRepo.findById(id);
+    	if(result.isPresent()) {
+    		UserDTO user = mapper.map(result.get(), UserDTO.class);
+    		if(user.getPassword().equals(password)) {
+    			return true;
+    		}
+    		else {
+    			throw new WeCareException("COACH_NOT_FOUND", HttpStatus.OK.value());
+    		}
+    	}
+    	else {
+    		throw new WeCareException("COACH_NOT_FOUND", HttpStatus.OK.value());
+    	}
     }
 
 }
