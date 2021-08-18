c287a8c9915ad0bde93e5ce21570dc1a9593af63
Skipper609 <sudhanva000@gmail.com>
2021-08-18 10:48:04 +0530

    added partial userlogin part

--- userMS/src/main/java/com/infosys/userms/service/UserService.java
+++ userMS/src/main/java/com/infosys/userms/service/UserService.java
@@ -1,9 +1,11 @@
 package com.infosys.userms.service;
 
+import com.infosys.userms.dto.LoginDTO;
 import com.infosys.userms.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
-UserDTO getUserById(String id);
+	UserDTO getUserById(String id);
+	Boolean userLogin(LoginDTO login);
 }
